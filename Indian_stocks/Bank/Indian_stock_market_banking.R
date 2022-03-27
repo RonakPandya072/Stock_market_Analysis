dcd0efceb757ffb0947ee40922fae1173aa7e731
Ronak Pandya <66405302+RonakPandya072@users.noreply.github.com>
2022-03-27 17:37:50 +0530

    Add files via upload

--- Indian_stocks/Bank/Indian_stock_market_banking.R
+++ Indian_stocks/Bank/Indian_stock_market_banking.R
@@ -3,6 +3,8 @@ library("PortfolioAnalytics")
 library("quantmod")
 library("Quandl")
 library("DEoptim")
+library("plotly")
+
 
 #**********************"Indian Stock Market analysis"*******************************************
 #Banking sector stocks analysis
@@ -11,6 +13,20 @@ library("DEoptim")
 #SBI
 
 getSymbols('SBIN.NS', src='yahoo', from="2007-01-01", to="2022-01-01")
+
+#candlestick_chart
+df <- data.frame(Date=index(SBIN.NS),coredata(SBIN.NS))
+df <- tail(df, 100)
+
+fig <- df %>% plot_ly(x = ~Date, type="candlestick",
+                      open = ~SBIN.NS.Open, close = ~SBIN.NS.Close,
+                      high = ~SBIN.NS.High, low = ~SBIN.NS.Low) 
+fig <- fig %>% layout(title = "Candlestick Chart SBI",
+                      xaxis = list(rangeslider = list(visible = F)))
+
+fig
+
+
 sbi = SBIN.NS[,6] #here we are taking adjusted price
 #columns: [open, high, low, close, volume, adjusted]
 msbi=monthlyReturn(sbi,type='arithmetic')
@@ -21,6 +37,19 @@ charts.PerformanceSummary(msbi)
 #HDFC
 
 getSymbols('HDFCBANK.BO', src='yahoo', from="2007-01-01", to="2022-01-01")
+
+#candlestick_chart
+df <- data.frame(Date=index(HDFCBANK.BO),coredata(HDFCBANK.BO))
+df <- tail(df, 100)
+
+fig <- df %>% plot_ly(x = ~Date, type="candlestick",
+                      open = ~HDFCBANK.BO.Open, close = ~HDFCBANK.BO.Close,
+                      high = ~HDFCBANK.BO,High, low = ~HDFCBANK.BO.Low) 
+fig <- fig %>% layout(title = "Candlestick Chart HDFC",
+                      xaxis = list(rangeslider = list(visible = F)))
+
+fig
+
 hdfc = HDFCBANK.BO[,6] #here we are taking adjusted price\
 mhdfc=monthlyReturn(hdfc,type='arithmetic')
 colnames(mhdfc) = "mhdfcl"
@@ -30,6 +59,19 @@ charts.PerformanceSummary(mhdfc)
 #ICICI
 
 getSymbols('ICICIBANK.BO', src='yahoo', from="2007-01-01", to="2022-01-01")
+
+#candlestick_chart
+df <- data.frame(Date=index(ICICIBANK.BO),coredata(ICICIBANK.BO))
+df <- tail(df, 100)
+
+fig <- df %>% plot_ly(x = ~Date, type="candlestick",
+                      open = ~ICICIBANK.BO.Open, close = ~ICICIBANK.BO.Close,
+                      high = ~ICICIBANK.BO.High, low = ~ICICIBANK.BO.Low) 
+fig <- fig %>% layout(title = "Candlestick Chart ICICI",
+                      xaxis = list(rangeslider = list(visible = F)))
+
+fig
+
 icici = ICICIBANK.BO[,6] #here we are taking adjusted price\
 micici=monthlyReturn(icici,type='arithmetic')
 colnames(micici) = "micici"
@@ -39,6 +81,19 @@ charts.PerformanceSummary(micici)
 #IDFC
 
 getSymbols('IDFC.NS', src='yahoo', from="2007-01-01", to="2022-01-01")
+
+#candlestick_chart
+df <- data.frame(Date=index(IDFC.NS),coredata(IDFC.NS))
+df <- tail(df, 100)
+
+fig <- df %>% plot_ly(x = ~Date, type="candlestick",
+                      open = ~IDFC.NS.Open, close = ~IDFC.NS.Close,
+                      high = ~IDFC.NS.High, low = ~IDFC.NS.Low) 
+fig <- fig %>% layout(title = "Candlestick Chart IDFC",
+                      xaxis = list(rangeslider = list(visible = F)))
+
+fig
+
 idfc = IDFC.NS[,6] #here we are taking adjusted price\
 midfc=monthlyReturn(idfc,type='arithmetic')
 colnames(midfc) = "midfc"
@@ -48,6 +103,19 @@ charts.PerformanceSummary(midfc)
 #Axis 
 
 getSymbols('AXISBANK.BO', src='yahoo', from="2007-01-01", to="2022-01-01")
+
+#candlestick_chart
+df <- data.frame(Date=index(AXISBANK.BO),coredata(AXISBANK.BO))
+df <- tail(df, 100)
+
+fig <- df %>% plot_ly(x = ~Date, type="candlestick",
+                      open = ~AXISBANK.BO.Open, close = ~AXISBANK.BO.Close,
+                      high = ~AXISBANK.BO.High, low = ~AXISBANK.BO.Low) 
+fig <- fig %>% layout(title = "Candlestick Chart Axis",
+                      xaxis = list(rangeslider = list(visible = F)))
+
+fig
+
 axisb = AXISBANK.BO[,6] #here we are taking adjusted price\
 maxis=monthlyReturn(axisb,type='arithmetic')
 colnames(maxis) = "maxis"
